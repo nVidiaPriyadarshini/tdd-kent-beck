12be6e34cd4a6108cedfc3c9be4ea16333b519c2
nVidia1 <galnextcomp@gmail.com>
2017-09-01 14:31:44 +0530

    Multiplication Test

---
+++ src/main/java/Dollar.java
@@ -0,0 +1,14 @@
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+
+    }
+}
